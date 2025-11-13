3d2f1bba52751513bdd31b53e4d4ff7e83ba76c2
Kyutae Sim <ktsim01@gmail.com>
2025-11-12 23:35:51 -0500

    need sparse matrix backend support

--- python/needle/ops/sparse_ops.py
+++ python/needle/ops/sparse_ops.py
@@ -7,49 +7,32 @@ from ..autograd import SparseTensorOp, SparseTensor
 #                      Helper utils                       #
 ###########################################################
 
-def _is_sparse(x):
-    return sp.issparse(x)
-
 def _to_dense(x):
-    return x.toarray() if _is_sparse(x) else np.asarray(x)
-
-def _wrap_tensor(x):
-    if sp.issparse(x):
-        x = x.toarray()
-    return SparseTensor(x)
-
-
+    return np.asarray(x)
 
 ###########################################################
 #                  Public sparse wrappers                 #
 ###########################################################
 
 def sparse_add(a, b):
-    a, b = _wrap_tensor(a), _wrap_tensor(b)
     return SparseAdd()(a, b)
 
 def sparse_multiply(a, b):
-    a, b = _wrap_tensor(a), _wrap_tensor(b)
     return SparseMul()(a, b)
 
 def sparse_matmul(a, b):
-    a, b = _wrap_tensor(a), _wrap_tensor(b)
     return SparseMatMul()(a, b)
 
 def sparse_add_scalar(a, scalar):
-    a = _wrap_tensor(a)
     return SparseAddScalar(scalar)(a)
 
 def sparse_mul_scalar(a, scalar):
-    a = _wrap_tensor(a)
     return SparseMulScalar(scalar)(a)
 
 def sparse_transpose(a):
-    a = _wrap_tensor(a)
     return SparseTranspose()(a)
 
 def sparse_relu(a):
-    a = _wrap_tensor(a)
     return SparseReLU()(a)
 
 ###########################################################
@@ -60,8 +43,6 @@ class SparseAdd(SparseTensorOp):
     """Elementwise addition for sparse matrices."""
 
     def compute(self, a, b):
-        if _is_sparse(a) and _is_sparse(b):
-            return a + b
         return np.add(_to_dense(a), _to_dense(b))
 
     def gradient(self, out_grad, node):
@@ -73,8 +54,6 @@ class SparseMul(SparseTensorOp):
     """Elementwise multiplication for sparse matrices."""
 
     def compute(self, a, b):
-        if _is_sparse(a) and _is_sparse(b):
-            return a.multiply(b)
         return np.multiply(_to_dense(a), _to_dense(b))
 
     def gradient(self, out_grad, node):
@@ -88,12 +67,6 @@ class SparseMatMul(SparseTensorOp):
     """Matrix multiplication supporting sparse inputs."""
 
     def compute(self, a, b):
-        if _is_sparse(a) and _is_sparse(b):
-            return a.dot(b)
-        if _is_sparse(a):
-            return a.dot(_to_dense(b))
-        if _is_sparse(b):
-            return _to_dense(a).dot(b.toarray())
         return np.dot(_to_dense(a), _to_dense(b))
 
     def gradient(self, out_grad, node):
@@ -112,11 +85,7 @@ class SparseAddScalar(SparseTensorOp):
         self.scalar = scalar
 
     def compute(self, a):
-        if _is_sparse(a):
-            if self.scalar == 0:
-                return a.copy()
-            return a + self.scalar  # will densify
-        return _to_dense(a) + self.scalar
+        return a + self.scalar
 
     def gradient(self, out_grad, node):
         # d/dA (A + c) = 1
@@ -130,9 +99,7 @@ class SparseMulScalar(SparseTensorOp):
         self.scalar = scalar
 
     def compute(self, a):
-        if _is_sparse(a):
-            return a * self.scalar
-        return _to_dense(a) * self.scalar
+        return a * self.scalar
 
     def gradient(self, out_grad, node):
         # d/dA (c * A) = c
@@ -143,8 +110,6 @@ class SparseTranspose(SparseTensorOp):
     """Transpose sparse or dense input."""
 
     def compute(self, a):
-        if _is_sparse(a):
-            return a.transpose()
         return np.transpose(_to_dense(a))
 
     def gradient(self, out_grad, node):
@@ -154,25 +119,11 @@ class SparseTranspose(SparseTensorOp):
 
 class SparseReLU(SparseTensorOp):
     """ReLU activation for sparse tensors."""
-
     def compute(self, a):
-        if _is_sparse(a):
-            a_coo = a.tocoo()
-            mask = a_coo.data > 0
-            if mask.sum() == 0:
-                return sp.csr_matrix(a.shape)
-            return sp.coo_matrix(
-                (a_coo.data[mask], (a_coo.row[mask], a_coo.col[mask])),
-                shape=a.shape,
-            ).tocsr()
         return np.maximum(_to_dense(a), 0)
 
     def gradient(self, out_grad, node):
         """Gradient: mask out gradients where input <= 0."""
         a = node.inputs[0]
-        if _is_sparse(a):
-            dense_a = a.toarray()
-            mask = (dense_a > 0).astype(float)
-            return out_grad * mask
         mask = (_to_dense(a) > 0).astype(float)
         return out_grad * mask
